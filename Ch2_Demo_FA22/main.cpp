9f4c08cb4dfb0497987a7940ade46e503b8d1966
BuschMath <busch@iowacentral.edu>
2022-08-31 10:45:03 -0500

    Vars Demo

--- Ch2_Demo_FA22/main.cpp
+++ Ch2_Demo_FA22/main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
+#include <string>
 
 // Function prototype
 void TestFunction();
 
+const std::string m = "Monday";
+
+
 int main()
 {
+	const std::string TuesdayIsADayOfTheWeek = "Tuesday";
+
 	// Function call
 	TestFunction();
 	std::cout << "Test";
 	// Function call
 	TestFunction();
+
+	std::cout << std::endl << m << TuesdayIsADayOfTheWeek;
 
 	return 0;
 }
@@ -18,4 +26,6 @@ int main()
 void TestFunction()
 {
 	std::cout << "In function";
+	std::cout << m;
+	//std::cout << TuesdayIsADayOfTheWeek; Not valid
 }
